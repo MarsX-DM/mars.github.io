f7795e1ab77c2453b9a7e0da8f3fe54b98c2e920
徐骄 <xujiao@cvte.com>
2019-12-12 00:01:41 +0800

    * Remove Page: MM

--- TC.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: UTF-8 -*-
-# @Time     : 12/11/19 13:25 PM
-# @Author   : Mars.Xu
-
-print("Hello MM!")
--- test3.py
+++
@@ -1,8 +0,0 @@
-
-import random
- 
-number = random.randint(1,10)
- 
-f = open('number.txt','w')
-f.write(str(number))
-f.close()
